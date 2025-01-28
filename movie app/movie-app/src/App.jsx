a149b1755a61016f93c1dea1594b07b3de4ca6f1
jaivatsan-elango-sundaram <je.sundaram@gmail.com>
2025-01-27 21:34:46 -0500

    continued wokring on search functionality

--- movie app/movie-app/src/App.jsx
+++ movie app/movie-app/src/App.jsx
@@ -5,10 +5,16 @@
 //type rafce
 
 //like said before, we are using appwrite for theis project
-import React from 'react'
+//make sure to import all things used like the useState
+import React, {useState} from 'react'
 //make sure to import any components created
 import Search from "./components/Search.jsx";
+//importing useState from react
+
 const App = () => {
+    //creating a state for a search term (searching for a movie)
+    const [searchTerm, setSearchTerm] = useState('men');
+    //ONLY MUTATE THE VALUES OF THE VARIABLES USING THE SETTER FUNCTIONS ABOVE 
     return (
         //starting top down from the header
         //adding a span tag and give it a classname - this corresponds to some css styling attributes in index.css
@@ -20,7 +26,10 @@ const App = () => {
                     <h1>Find <span className = "text-gradient">Movies </span>You'll Enjoy Without the Hassle </h1>
                 </header>
                 {/* rendering in the search component*/}
-                <Search />
+                {/*we are passing in state fields as props*/}
+                {/*props are inputs we pass into a component, like parameter we pass into a function*/}
+                {/* as a user searches for something, we can pass that search term into search */}
+                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
 
             </div>
         </main>
--- movie app/movie-app/src/components/Search.jsx
+++ movie app/movie-app/src/components/Search.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
-const Search = () => {
+//can accept the props here
+const Search = (props) => {
+    //props SHOULD NOT BE CHANGED BY THE CHILD COMPONENT
     return (
         //for now, just making sure it appears
-        <div className = "text-white">Search</div>
+        //can get specific components of the props s
+        <div className = "text-white">{props.searchTerm}</div>
     )
 }
 export default Search
