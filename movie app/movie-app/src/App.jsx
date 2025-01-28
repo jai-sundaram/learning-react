904d36065aa883848148cb3329a5be75aa7d8434
jaivatsan-elango-sundaram <je.sundaram@gmail.com>
2025-01-27 23:43:52 -0500

    continued working on the application

--- movie app/movie-app/src/App.jsx
+++ movie app/movie-app/src/App.jsx
@@ -4,21 +4,23 @@
 //go back to app.jsx
 //type rafce
 
+//IN JS<, SEMICOLONS ARE NOT NECESSARY BUT ADDING THEM IS A GOOD PRACTICE AS IT REDUCES CONFUSION
+
 //like said before, we are using appwrite for theis project
 //make sure to import all things used like the useState
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 //make sure to import any components created
 import Search from "./components/Search.jsx";
 //importing useState from react
 //ctrl + c to stop it from running
 
 //api keys - dont put it in the main file where it can be easily seen
-//create '.env.local' file in the src folder
+//create a file called '.env.local' in the project folder, on the same level (NOT IN) the src folder 
 //follow a similair variable naming convention when storing the key
 //then restart the terminal for it to recognize it
 
 //using the api to get a list of the most popular movies
-const API_BASE_URL = "https://api.themoviedb.org/3/";
+const API_BASE_URL = "https://api.themoviedb.org/3";
 
 //getting acess to the api key
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -28,9 +30,10 @@ const API_OPTIONS = {
     //get request
     method: 'GET',
     //accepting data in json format
-    header: {
+    headers: {
         accept: 'application/json',
-        //need to autnethicate the api key 
+        //need to autnethicate the api key
+        authorization: `Bearer ${API_KEY}`
     }
 }
 const App = () => {
@@ -39,10 +42,39 @@ const App = () => {
     //ONLY MUTATE THE VALUES OF THE VARIABLES USING THE SETTER FUNCTIONS ABOVE
 
 
+    //display any erros on the webpage
+    const [errorMessage, setErrorMessage] = useState(null)
+    //declaring a function that will allow us to fetch the movies
+    const fetchMovies = async() =>
+    {
+        try{
+            //creating the endpoint - where the data will be retrieved from
+            const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+            //gettting the response
+            const response = await fetch(endpoint, API_OPTIONS)
+            //fetch is a built js function that allows u to make http requests like get and post
+            //check if the http request was sucessful
+            //if it is not fine, throw a error
+            if(!response.ok){
+                throw new Error("Failed to fetch movies")
+            }
+            //otherwise, just get the data in the json format
+            const data = await response.json();
+        }
+        catch(error){
+            console.log('error fetching movies')
+            //setting the error message if we catch any error messages
+            setErrorMessage('Error fetching movies. Please try again later')
+        }
+    }
     ////using the useEffect hook to fetch the movies
     //make sure that it only loads at the start, by making sure it only runs at the start
     //to do so, passing in an empty dependency array
     useEffect(() => {
+        //we want to load the movies as soon as we come inside the website
+        //so, call the fetchMovies method
+        fetchMovies();
+
 
     }, [])
     return (
@@ -54,15 +86,21 @@ const App = () => {
                 <header>
                     <img src = "./hero.png" alt = "hero-banner" />
                     <h1>Find <span className = "text-gradient">Movies </span>You'll Enjoy Without the Hassle </h1>
+                    {/* rendering in the search component*/}
+                    {/*we are passing in state fields as props*/}
+                    {/*props are inputs we pass into a component, like parameter we pass into a function*/}
+                    {/* as a user searches for something, we can pass that search term into search */}
+                    <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
                 </header>
-                {/* rendering in the search component*/}
-                {/*we are passing in state fields as props*/}
-                {/*props are inputs we pass into a component, like parameter we pass into a function*/}
-                {/* as a user searches for something, we can pass that search term into search */}
-                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
-                <h1>{searchTerm}</h1>
+                {/*creating a section for all the movies display*/}
+                <section className = "movies">
+                    <h2>All movies </h2>
+                    {/*if there is an error messages, render a paragraph tag and display that error message*/}
+                    {errorMessage && <p className = "text-red-500">{errorMessage}</p>}
+                </section>
             </div>
         </main>
     )
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNjM1ZGM5MDdlOGVjODMxOGFiOWM3ODVjOWFjNzM1MCIsIm5iZiI6MTczODAzMzg0Ny40NTcsInN1YiI6IjY3OTg0YWI3OWEzMGE4NWIyNzI0MmE2MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VdzX7TDQq4s4Di5ivnlOELgXigjaLgcgNcQXUwMDPAM'
 }
 export default App
